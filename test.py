8c48c1a8a40049d3b8a62b1de71ed8cfb2925736
Bernhard <b_walter@arcor.de>
2024-03-24 13:47:54 +0100

    add deserialize directly

--- test.py
+++ test.py
@@ -1,14 +1,36 @@
-from time import time
-from ocp_tessellate.ocp_utils import deserialize
-
+import io
+import os
 import sys
+import tempfile
+from time import time
+
+from OCP.BinTools import BinTools
+from OCP.TopoDS import TopoDS_Shape
+
 sys.path.append("build")
 
 import ocp_tessellate_native as tess
 
 
+def deserialize(buffer):
+    if buffer is None:
+        return None
+
+    shape = TopoDS_Shape()
+    try:
+        bio = io.BytesIO(buffer)
+        BinTools.Read_s(shape, bio)
+    except Exception:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            filename = os.path.join(tmpdirname, "shape.brep")
+            with open(filename, "wb") as fd:
+                fd.write(buffer)
+            BinTools.Read_s(shape, filename)
+    return shape
+
+
 def decompose(array, indexes, flatten=False):
-    result =[]
+    result = []
     s = 0
     for e in indexes:
         e2 = s + e
@@ -19,26 +41,28 @@ def decompose(array, indexes, flatten=False):
         s = e2
     return result
 
+
 def tessellate(obj, deflection, angular_tolerance, parallel):
-    m =  tess.tessellate(obj, deflection, angular_tolerance, parallel, False, True)
-    
+    m = tess.tessellate(obj, deflection, angular_tolerance, parallel, False, True)
+
     t = time()
     tr = decompose(m.triangles.reshape(-1, 3), m.triangles_per_face, True)
-    print("Reshape tessellation", int(1000*(time() - t)), "ms")
+    print("Reshape tessellation", int(1000 * (time() - t)), "ms")
 
     t = time()
     sg = decompose(m.segments.reshape(-1, 2, 3), m.segments_per_edge)
-    print("Reshape edges", int(1000*(time() - t)), "ms")
+    print("Reshape edges", int(1000 * (time() - t)), "ms")
 
     return {
         "vertices": m.vertices,
         "normals": m.normals,
-        "triangles": tr,   
-        "face_types": m.face_types, 
+        "triangles": tr,
+        "face_types": m.face_types,
         "edges": sg,
         "edge_types": m.edge_types,
-        "obj_vertices": m.obj_vertices
+        "obj_vertices": m.obj_vertices,
     }
+
 
 file, acc, show = "examples/b123.brep", 0.002, True
 # file, acc, show = "examples/rc.brep", 0.19, False
@@ -52,6 +76,7 @@ tt = time()
 mesh = tessellate(obj, acc, 0.3, parallel=True)
 
 import json
+
 if show:
     print("vertices:", mesh["vertices"])
     print("normals:", mesh["normals"])
